b4ad83979d11e1adc7ec48ebdd5c9d925f3fcb4a
giuliojiang <giuliojiang@gmail.com>
2018-11-18 15:44:55 +0000

    Create 2 projects and 1 shared library

---
+++ hub/projects/lib1/src/lib/lib1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-lib1',
+  templateUrl: './lib1.component.html',
+  styles: []
+})
+export class Lib1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
